711180a504cc31c7e7313688351a7004abf3faab
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-06-29 18:48:04 +0800

    add template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['as' => 'admin.', 'prefix' => 'admin', 'middleware' => ['auth']], function(){
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 });
